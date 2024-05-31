37ad1e2e802a03bde1798f2e60bb34a8575b42de
EricKart <itsary1020@yahoo.com>
2024-06-01 04:23:54 +0530

    added few features to the home-page including nav-bar

---
+++ webapp/forms.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms.widgets import PasswordInput, TextInput
+
+
+#-- Register/Create a user
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', "password2"]
+        
+        
+#--Login User form---
+class LoginForm():
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
+    
+
+
+
+
+
+
+
